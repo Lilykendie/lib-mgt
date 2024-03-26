ad695d79b3cd19b5f27450f2a133606519e2fe84
Lilykendie <akinyililian993@gmail.com>
2024-03-26 13:00:30 +0300

    initial

---
+++ about_us.php
@@ -0,0 +1,307 @@
+<?php include ('header.php'); ?>
+
+        <div class="page-title">
+            <div class="title_left">
+                <h3>
+					<small>Home /</small> About Us
+                </h3>
+            </div>
+        </div>
+        <div class="clearfix"></div>
+ 
+        <div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+							<br />
+							<br />
+                    <div class="x_title">
+                        <h2><i class="fa fa-info"></i> Developer Information</h2>
+                        <ul class="nav navbar-right panel_toolbox">
+                            <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
+                        <!-- If needed 
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <i class="fa fa-wrench"></i>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">Settings 1</a></li>
+                                    <li><a href="#">Settings 2</a></li>
+                                </ul>
+                            </li>
+						-->
+                            <li><a class="close-link"><i class="fa fa-close"></i></a></li>
+                        </ul>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="x_content">
+                        <!-- content starts here -->
+
+                <div class="" role="tabpanel" data-example-id="togglable-tabs">
+                    <ul id="myTab" class="nav nav-tabs bar_tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#tab_content1" id="home-tab" role="tab" data-toggle="tab" aria-expanded="true">lilian </a>
+                        </li>
+                        <li role="presentation" class=""><a href="#tab_content2" role="tab" id="profile-tab" data-toggle="tab"  aria-expanded="false">lilian </a>
+                        </li>
+                        <li role="presentation" class=""><a href="#tab_content3" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false">james chege</a>
+                        </li>
+                        <li role="presentation" class=""><a href="#tab_content3" role="tab" id="profile-tab2" data-toggle="tab" aria-expanded="false"></a>
+                        </li>
+                    </ul>
+                    <div id="myTabContent" class="tab-content">
+                        <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="home-tab">
+							<fieldset class="pull-left" style="margin-left:230px;">
+								<legend>Personal Information</legend>
+									<dt>
+										Age
+										<ul>
+											<li>
+											23 years
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Civil Staus
+										<ul>
+											<li>
+											Single
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Date of Birth
+										<ul>
+											<li>
+											April 28, 1996
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Place of Birth
+										<ul>
+											<li>
+											kisumu
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Citizenship
+										<ul>
+											<li>
+											kenyan
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Sex
+										<ul>
+											<li>
+											female
+											</li>
+										</ul>
+									</dt>
+							</fieldset>
+							<fieldset class="pull-right" style="margin-right:200px;">
+								<legend>Education</legend>
+									<dt>
+										Course
+										<ul>
+											<li>
+											Bachelor of Science in Information Systems
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Egerton university
+										<ul>
+											<li>
+											lilian
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										School Year
+										<ul>
+											<li>
+											2021 – 2024
+											</li>
+										</ul>
+									</dt>
+							</fieldset>
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
+						<div style="">
+							<fieldset class="pull-left" style="margin-left:230px;">
+								<legend>Personal Information</legend>
+									<dt>
+										Age
+										<ul>
+											<li>
+											20 years old
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Civil Staus
+										<ul>
+											<li>
+											Single
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Date of Birth
+										<ul>
+											<li>
+											april 24, 1996
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Place of Birth
+										<ul>
+											<li>
+										Nyanza
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Citizenship
+										<ul>
+											<li>
+										kenyan
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Sex
+										<ul>
+											<li>
+											FEMale
+											</li>
+										</ul>
+									</dt>
+							</fieldset>
+							<fieldset class="pull-right" style="margin-right:200px;">
+								<legend>Education</legend>
+									<dt>
+										Course
+										<ul>
+											<li>
+											Bachelor of Science in Information TECHNOLOGY
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										School
+										<ul>
+											<li>
+											EGERTON UNIVERSITY
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										School Year
+										<ul>
+											<li>
+											2022 – 2025
+											</li>
+										</ul>
+									</dt>
+							</fieldset>
+						</div>
+                        </div>
+                        <div role="tabpanel" class="tab-pane fade" id="tab_content3" aria-labelledby="profile-tab">
+						<div style="">
+							<fieldset class="pull-left" style="margin-left:230px;">
+								<legend>Personal Information</legend>
+									<dt>
+										Age
+										<ul>
+											<li>
+											32 years old
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Civil Staus
+										<ul>
+											<li>
+											Single
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Date of Birth
+										<ul>
+											<li>
+											July 2, 1992
+
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Place of Birth
+										<ul>
+											<li>
+										NAIROBI
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Citizenship
+										<ul>
+											<li>
+											Kenya
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										Sex
+										<ul>
+											<li>
+											male
+											</li>
+										</ul>
+									</dt>
+							</fieldset>
+							<fieldset class="pull-right" style="margin-right:200px;">
+								<legend>Education</legend>
+									<dt>
+										Course
+										<ul>
+											<li>
+											Bachelor of computer  Science
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										School
+										<ul>
+											<li>
+											st. pauls university
+											</li>
+										</ul>
+									</dt>
+									<dt>
+										School Year
+										<ul>
+											<li>
+											2015 – 2016
+											</li>
+										</ul>
+									</dt>
+							</fieldset>
+						</div>
+                        </div>
+                     
+                        </div>
+                    </div>
+                </div>
+						
+                        <!-- content ends here -->
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<?php include ('footer.php'); ?>
